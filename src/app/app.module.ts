e5f8bbc5075a9f000e7653dc2d0ca6c39b703bce
soumitra kar <soumitra.kar@wipro.com>
2017-06-17 23:41:08 +0530

    bootstrap setup and documentation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent }  from './app.component';
+import { LeftPanelComponent }  from './leftpanel/leftpanel.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, LeftPanelComponent ],
+  imports:      [ BrowserModule, NgbModule.forRoot()],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
